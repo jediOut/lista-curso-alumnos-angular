4cac38b97a08af9d173a75a7e842838260533bb3
JediOut <jedi.viera@gmail.com>
2024-11-29 17:22:33 -0600

    Proyecto de lista de alumnos con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TablaAlumnosComponent } from './components/tabla-alumnos/tabla-alumnos.component';
+import { TablaCursosComponent } from './components/tabla-cursos/tabla-cursos.component';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TablaAlumnosComponent, TablaCursosComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-cursos';
+  cursos: boolean = true;
+  handleCambio() {
+    this.cursos = !this.cursos;
+  }
 }
